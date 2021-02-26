d95a9c22bd1ce2ee4cbe808209f29d181e10113e
Kevin Andrus <kevin.andrus@gmail.com>
2021-02-26 13:45:56 -0500

    Add tests for Piece in model

--- model/tests.py
+++ model/tests.py
@@ -1,6 +1,13 @@
 from tile import Tile
+from piece import Piece
+
+image_name = 'token_1.png'
+piece = Piece(image_name)
+assert piece.image_name == image_name
 
 tile = Tile()
 assert tile.piece == None
+tile.piece = piece
+assert tile.piece == piece
 
 print("Model unit tests complete. No errors found.")
