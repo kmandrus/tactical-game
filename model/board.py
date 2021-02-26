baa663fa40bdc404b350e96f2a61443753b3c19e
Kevin Andrus <kevin.andrus@gmail.com>
2021-02-26 15:39:42 -0500

    Add Board class to model

---
+++ model/board.py
@@ -0,0 +1,31 @@
+from tile import Tile
+from piece import Piece
+
+def to_empty_grid(pos_list):
+    return {pos: Tile() for pos in pos_list}
+
+class Board:
+    def __init__(self, grid):
+        self.__grid = grid
+    
+    def get_tile(self, pos):
+        return self.__grid[pos]
+    
+    def add_piece(self, piece, pos):
+        self.get_tile(pos).piece = piece
+    
+    def remove_piece(self, pos):
+        self.get_tile(pos).piece = None
+
+    def move_piece(self, start, end):
+        if (piece := self.get_tile(start).piece) and self.is_empty_at(end):
+            self.remove_piece(start)
+            self.add_piece(piece, end)
+        else:
+            raise Exception(f"Error moving piece from {start} to {end}")
+
+    def get_piece_at(self, pos):
+        return self.get_tile(pos).piece
+
+    def is_empty_at(self, pos):
+        return self.get_tile(pos).piece == None
--- model/tests.py
+++ model/tests.py
@@ -1,15 +1,66 @@
 from tile import Tile
 from piece import Piece
+from board import Board
+from board import to_empty_grid
 
+#Piece Tests
 name = 'Ajani'
 piece = Piece(name)
 assert piece.name == name
 
+#Tile Tests
 tile = Tile()
 assert tile.piece == None
 tile.piece = piece
 assert tile.piece == piece
 tile.piece = None
 assert tile.piece == None
+
+#Board Tests
+#board#to_empty_grid
+positions = [(x, y) for x in range(10) for y in range(10)]
+grid = to_empty_grid(positions)
+for pos in positions:
+    assert grid[pos]
+    assert not grid[pos].piece
+
+board = Board(grid)
+pos = (3, 3)
+other_pos = (2, 5)
+pos_outside_board = (12, 12)
+#Board#get_tile
+assert board.get_tile(pos)
+try:
+    board.get_tile(pos_outside_board)
+except KeyError:
+    pass
+assert not board.get_tile(pos).piece
+#Board#add_piece
+mock_piece = "mock"
+board.add_piece(mock_piece, pos)
+assert board.get_tile(pos).piece == mock_piece
+#Board#get_piece_at
+assert board.get_piece_at(pos) == mock_piece
+#Board#is_empty_at
+assert board.is_empty_at(other_pos)
+assert not board.is_empty_at(pos)
+#Board#remove_piece
+board.remove_piece(pos)
+assert not board.get_piece_at(pos)
+assert not board.get_tile(other_pos).piece
+assert board.is_empty_at(pos)
+#Board#move_piece
+board.add_piece(mock_piece, pos)
+board.move_piece(pos, other_pos)
+assert not board.get_tile(pos).piece
+assert not board.get_piece_at(pos)
+assert board.is_empty_at(pos)
+assert board.get_tile(other_pos).piece == mock_piece
+assert board.get_piece_at(other_pos) == mock_piece
+assert not board.is_empty_at(other_pos)
+try:
+    board.move_piece(pos, other_pos)
+except Exception:
+    pass
 
 print("Model unit tests complete. No errors found.")
