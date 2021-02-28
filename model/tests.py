33d09e45b6bb2921537e51f026143904ec5eb4a8
Kevin Andrus <kevin.andrus@gmail.com>
2021-02-28 13:30:38 -0500

    Fix tests that expect  errors in model

--- model/tests.py
+++ model/tests.py
@@ -3,10 +3,12 @@ from piece import Piece
 from board import Board
 from board import to_empty_grid
 
+
 #Piece Tests
 name = 'Ajani'
 piece = Piece(name)
 assert piece.name == name
+
 
 #Tile Tests
 tile = Tile()
@@ -16,7 +18,9 @@ assert tile.piece == piece
 tile.piece = None
 assert tile.piece == None
 
+
 #Board Tests
+
 #board#to_empty_grid
 positions = [(x, y) for x in range(10) for y in range(10)]
 grid = to_empty_grid(positions)
@@ -28,27 +32,34 @@ board = Board(grid)
 pos = (3, 3)
 other_pos = (2, 5)
 pos_outside_board = (12, 12)
+
 #Board#get_tile
 assert board.get_tile(pos)
 try:
     board.get_tile(pos_outside_board)
+    raise AssertionError
 except KeyError:
     pass
 assert not board.get_tile(pos).piece
+
 #Board#add_piece
 mock_piece = "mock"
 board.add_piece(mock_piece, pos)
 assert board.get_tile(pos).piece == mock_piece
+
 #Board#get_piece_at
 assert board.get_piece_at(pos) == mock_piece
+
 #Board#is_empty_at
 assert board.is_empty_at(other_pos)
 assert not board.is_empty_at(pos)
+
 #Board#remove_piece
 board.remove_piece(pos)
 assert not board.get_piece_at(pos)
 assert not board.get_tile(other_pos).piece
 assert board.is_empty_at(pos)
+
 #Board#move_piece
 board.add_piece(mock_piece, pos)
 board.move_piece(pos, other_pos)
@@ -60,7 +71,9 @@ assert board.get_piece_at(other_pos) == mock_piece
 assert not board.is_empty_at(other_pos)
 try:
     board.move_piece(pos, other_pos)
+    raise AssertionError
 except Exception:
     pass
 
-print("Model unit tests complete. No errors found.")
+
+print("Model unit tests successful!")
