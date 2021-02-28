718aafeb7f12c5e7ba9fbacb0e2ed95df13c6775
Kevin Andrus <kevin.andrus@gmail.com>
2021-02-28 13:34:04 -0500

    Add demo that converts clicks into pixel and hex coordinates

---
+++ view/pix_to_hex_demo.py
@@ -0,0 +1,36 @@
+import pygame as pg
+
+from hex_board_view import HexBoardView
+
+def create_hex_pos_list(width, height):
+    positions = []
+    for x in range(width):
+        for y in range(height):
+            if x % 2 == 0:
+                positions.append((x, y * 2))
+            else:
+                positions.append((x, (y * 2) + 1))
+    return positions
+
+pg.init()
+
+DIMENSIONS = (800, 600)
+RADIUS = 32
+
+screen = pg.display.set_mode(DIMENSIONS)
+pg.display.set_caption("Pixel to Hexagonal Coordinate Conversion Demo")
+
+board = HexBoardView(screen, DIMENSIONS, RADIUS, create_hex_pos_list(18, 13))
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        if event.type == pg.MOUSEBUTTONUP:
+            pix_pos = pg.mouse.get_pos()
+            hex_pos = board.to_hex(pix_pos)
+            print(f"pix_pos: {pix_pos}, hex_pos: {hex_pos}")
+        screen.fill((64, 128, 64))
+        board.draw()
+        pg.display.update()
